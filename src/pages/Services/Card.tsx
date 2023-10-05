69f83b0d15ea0e48004a5d795d42edb8ffc12c99
VysochanskiyS <visosensey@gmail.com>
2023-10-05 08:40:31 +0300

    upd: updated text on services cards

--- src/pages/Services/Card.tsx
+++ src/pages/Services/Card.tsx
@@ -21,55 +21,55 @@ const Card: React.FunctionComponent<IServiceCard> = () => {
           <ImageHoverComponent
             image={DesignCard}
             title={'Custom design'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Upload your designs via our portal, and our skilled team will bring them to life'}
           />
           <ImageHoverComponent
             image={StoneSetting}
             title={'Stone Setting'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'From prong to intricate French pavé, our skilled jewelers ensure flawless settings, perfecting every detail.'}
           />
         </Styled.GroupCardTop>
         <Styled.GroupCardBottom>
           <ImageHoverComponent
             image={Cad}
             title={'CAD'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Our CAD designers are jewelers, crafting designs that are both beautiful and practical, with a keen eye for precision.'}
           />
           <ImageHoverComponent
             image={Engraving}
             title={'Engraving'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Choose from hand, laser, or etching for exquisite detailing by our top engraver.'}
           />
         </Styled.GroupCardBottom>
         <Styled.GroupCardTop>
           <ImageHoverComponent
             image={Repair}
             title={'Repair'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'We offer a wide range of repair services, from minor fixes to major restorations, guaranteeing your jewelry is in expert hands.'}
           />
           <ImageHoverComponent
             image={Casting}
             title={'Casting'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'We offer casting services for various metals, catering to both small and large items. Need it fast? Our expedited orders ensure you get your creations on time.'}
           />
         </Styled.GroupCardTop>
         <Styled.GroupCardBottom >
           <ImageHoverComponent
             image={Manufacturing}
             title={'Manufacturing'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'We offer start-to-finish manufacturing services, ensuring top-notch quality in every step of the process for your small-scale production needs."'}
           />
           <ImageHoverComponent
             image={Wax}
             title={'Wax Prototype'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Our wax growing services deliver impeccable smoothness. With a lightning-fast 24-hour turnaround, your designs come to life with precision and speed."'}
           />
         </Styled.GroupCardBottom>
         <Styled.GroupCardTop>
           <ImageHoverComponent
             image={Photography}
             title={'Photography'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Our state-of-the-art setup and skilled photographer capture the essence of your jewelry, ensuring stunning visuals'}
           />
           <img src={Ask} />
         </Styled.GroupCardTop>
@@ -79,34 +79,34 @@ const Card: React.FunctionComponent<IServiceCard> = () => {
           <ImageHoverComponent
             image={Cad}
             title={'CAD'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Our CAD designers are jewelers, crafting designs that are both beautiful and practical, with a keen eye for precision.'}
           />
           <ImageHoverComponent
             image={DesignCard}
             title={'Custom design'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Upload your designs via our portal, and our skilled team will bring them to life'}
           />
           <ImageHoverComponent
             image={Engraving}
             title={'Engraving'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Choose from hand, laser, or etching for exquisite detailing by our top engraver.'}
           />
         </Styled.GroupCardBottom>
         <Styled.GroupCardTop>
           <ImageHoverComponent
             image={Repair}
             title={'Repair'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'We offer a wide range of repair services, from minor fixes to major restorations, guaranteeing your jewelry is in expert hands.'}
           />
           <ImageHoverComponent
             image={Casting}
             title={'Casting'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'We offer casting services for various metals, catering to both small and large items. Need it fast? Our expedited orders ensure you get your creations on time.'}
           />
           <ImageHoverComponent
             image={StoneSetting}
             title={'Stone Setting'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'From prong to intricate French pavé, our skilled jewelers ensure flawless settings, perfecting every detail.'}
           />
           <img src={Ask} />
         </Styled.GroupCardTop>
@@ -114,17 +114,17 @@ const Card: React.FunctionComponent<IServiceCard> = () => {
           <ImageHoverComponent
             image={Manufacturing}
             title={'Manufacturing'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'We offer start-to-finish manufacturing services, ensuring top-notch quality in every step of the process for your small-scale production needs."'}
           />
           <ImageHoverComponent
             image={Photography}
             title={'Photography'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Our state-of-the-art setup and skilled photographer capture the essence of your jewelry, ensuring stunning visuals'}
           />
           <ImageHoverComponent
             image={Wax}
             title={'Wax Prototype'}
-            text={'We offer Rush or date specific service to get your order done.'}
+            text={'Our wax growing services deliver impeccable smoothness. With a lightning-fast 24-hour turnaround, your designs come to life with precision and speed."'}
           />
         </Styled.GroupCardBottom>
       </Styled.WrapperSmall>
