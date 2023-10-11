b0df1edca139bcf6826d979b85977a4b1c6a9ef6
VysochanskiyS <visosensey@gmail.com>
2023-10-11 07:42:43 +0300

    fixed: footer

--- src/components/QuestionAnswer/QuestionAnswer.tsx
+++ src/components/QuestionAnswer/QuestionAnswer.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.scss";
+import {useCallback, useState} from "react";
 
 const QuestionAnswer = ({
   summary,
@@ -7,6 +8,8 @@ const QuestionAnswer = ({
   summary: string;
   question: string;
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   //@ts-ignore
   function closeOtherDetails(targetDetail) {
     details.forEach((detail) => {
@@ -23,10 +26,22 @@ const QuestionAnswer = ({
     });
   });
 
+
+  const handleOnClickQuestion = useCallback(() => {
+    console.log(isOpen);
+
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  }, [isOpen]);
+
   return (
     <details
       style={{ color: "var(--faq-textContent)" }}
       className={styles.details}
+      onClick={handleOnClickQuestion}
     >
       <summary className={styles.summary}>{summary}</summary>
       <p className={styles.text}>{question}</p>
